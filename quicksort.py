b678abaad54d6a529d1f68c0ebdce688b040920b
Victor Chiu <victor.chiu@codeminer42.com>
2012-08-13 02:28:40 -0300

    Adding quicksort algo with list comprehensions

---
+++ quicksort.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import timeit
+
+
+def quicksort(l):
+    if l == []:
+        return []
+    else:
+        pivot = l[0]
+        lesser = quicksort([x for x in l[1:] if x < pivot])
+        greater = quicksort([x for x in l[1:] if x >= pivot])
+        return lesser + [pivot] + greater
+
+if (__name__ == "__main__"):
+    try:
+        t = timeit.Timer(setup='from __main__ import quicksort', stmt="quicksort(sys.argv[1].replace(' ', '').split(','))")
+        print t.timeit(1)
+        print quicksort(sys.argv[1].replace(' ', '').split(','))
+    except:
+        print 'Usage: python file_name.py LIST_TO_SORT'
